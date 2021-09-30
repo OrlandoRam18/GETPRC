73c229506681fd3407fdec4d6f9b60e316a9c703
OrlandoRam18 <91481497+OrlandoRam18@users.noreply.github.com>
2021-09-29 22:58:18 -0500

    Main file of GETP
    Add file which is the main code for optimization, first version

---
+++ SDDP_OpValue.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+### Generation and expantion transmission planning includding reactive compensation
+###
+### Deterministic version SD
+###
+### 29/09/2021
+###
+### Update 1.000
+
+
+
+
+
+#-----------------------------------------------------------------------------------
+# Call data
+import pandas as pd
+DB = pd.read_excel("14 bus data.xlsx", sheet_name=None)
